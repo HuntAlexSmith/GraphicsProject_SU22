3c59b3c1b9a367eb0817ef1287c53a63bbe017f0
ZeeHunter0322 <huntalexsmith@gmail.com>
2022-05-12 14:22:54 -0700

    Initial
    SDL and GLAD should now be working with the project.

---
+++ DuckGFX/Source/Main.cpp
@@ -0,0 +1,11 @@
+//*****************************************************************************
+//	File:   Main.cpp
+//	Author: Hunter Smith
+//	Date:	05/12/2022
+//	Description: This is the main function file, where the program starts.
+//		Nothing too fancy should really be happening here
+//*****************************************************************************
+
+int main() {
+	return 0;
+}
